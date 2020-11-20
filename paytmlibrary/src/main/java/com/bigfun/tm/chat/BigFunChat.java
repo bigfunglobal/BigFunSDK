b1d0e99d9fe15593d310ec4985eeda895c8df1fd
lqq <495776706@qq.com>
2020-11-20 11:05:31 +0800

    添加keep

--- paytmlibrary/src/main/java/com/bigfun/tm/chat/BigFunChat.java
+++ paytmlibrary/src/main/java/com/bigfun/tm/chat/BigFunChat.java
@@ -1,12 +1,13 @@
 package com.bigfun.tm.chat;
 
 import android.content.Context;
+import android.support.annotation.Keep;
 
 import zendesk.chat.Chat;
 import zendesk.chat.ChatConfiguration;
 import zendesk.chat.ChatEngine;
 import zendesk.messaging.MessagingActivity;
-
+@Keep
 public class BigFunChat {
     private static final String ACCOUNT_KEY = "oYHRItbGeLakwycsAUxG8R0ZmooqeuIa";
     private static Context mContext;
@@ -19,19 +20,23 @@ public class BigFunChat {
      *
      * @param context
      */
+    @Keep
     public static void init(Context context) {
         mContext = context;
         Chat.INSTANCE.init(context, ACCOUNT_KEY);
     }
 
+    @Keep
     private static class InstanceHolder {
         private static BigFunChat INSTANCE = new BigFunChat();
     }
 
+    @Keep
     public static BigFunChat getInstance() {
         return InstanceHolder.INSTANCE;
     }
 
+    @Keep
     public void chat() {
         ChatConfiguration chatConfiguration = ChatConfiguration.builder()
                 .withPreChatFormEnabled(false)
